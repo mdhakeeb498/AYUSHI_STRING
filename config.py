64fc9f09c45ac37180f24b14f05824b9c5eb14e0
TEAM RIYA MUSIC <crazedpalak@gmail.com>
2024-09-25 21:46:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","21803165"))
-API_HASH = getenv("API_HASH","05e5e695feb30e25bef47484cc006da7")
+API_ID = int(getenv("API_ID","13404637"))
+API_HASH = getenv("API_HASH","a069bf02806468fe18427ab6b9a3bb6c")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","7403621976"))
+OWNER_ID = int(getenv("OWNER_ID","6283192619"))
 
-MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-MUST_JOIN = getenv("MUST_JOIN","WORLD_ALPHA")
+MONGO_DB_URI = getenv("mongodb+srv://DxLEGEND143:DxLEGEND143@dxlegend.oztipqk.mongodb.net/?retryWrites=true&w=majority&appName=DxLEGEND")
+MUST_JOIN = getenv("MUST_JOIN","KSD_BOT_NETWORK")
