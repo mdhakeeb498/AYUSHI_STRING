abb75191853e7ebd13da664823e87dfcc97939c8
TEAM RIYA MUSIC <crazedpalak@gmail.com>
2024-09-25 21:31:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = Client(
 )
 
 if __name__ == "__main__":
-    print("𝙰𝚕𝚙𝚑𝚊 𝚂𝚎𝚜𝚜𝚒𝚘𝚗 𝙶𝚎𝚗 𝚜𝚝𝚊𝚛𝚝𝚒𝚗𝚐...")
+    print("𝙺𝚜𝚍 𝚂𝚝𝚛𝚒𝚗𝚐 𝙶𝚎𝚗 𝚂𝚝𝚊𝚛𝚝𝚒𝚗𝚐...")
     try:
         app.start()
     except ApiIdInvalid:
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         raise
 
     uname = app.get_me().username
-    print(f"@{uname} NOW ALPHA SESSION GEN IS READY TO GEN SESSION")
+    print(f"@{uname} NOW KSD SESSION GEN IS READY TO GEN SESSION")
     
     idle()
     
