601e97a9f7ed881bf123b4bfe03d8789d3a7a823
TEAM RIYA MUSIC <crazedpalak@gmail.com>
2024-10-26 11:30:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID","13404637"))
 API_HASH = getenv("API_HASH","a069bf02806468fe18427ab6b9a3bb6c")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN","7158045860:AAFK9c1BRAQd2fM2Zyctbiz4y-iDFMVe8Rc")
 OWNER_ID = int(getenv("OWNER_ID","6283192619"))
 
 MONGO_DB_URI = getenv("mongodb+srv://DxLEGEND143:DxLEGEND143@dxlegend.oztipqk.mongodb.net/?retryWrites=true&w=majority&appName=DxLEGEND")
